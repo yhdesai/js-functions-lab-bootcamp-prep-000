2897ddc99cd41d4fc66341dd4ef57d0427cae6fa
yash desai <yhdesai99@ymail.com>
2019-08-23 17:08:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
+
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It'd ${days} days until ${holiday}!"  
+}
